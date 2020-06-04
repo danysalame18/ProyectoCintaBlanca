88ff870abb21111e609b3f00eb6acce25f5a98de
danysalame18 <65205086+danysalame18@users.noreply.github.com>
2020-06-04 15:56:01 -0500

    empece el js

---
+++ app.js
@@ -0,0 +1,11 @@
+
+var contaClicks = 0;
+var x = prueba
+
+function agregarCarrito(){
+    var img1 = document.getElementById("imagenCar1");
+    var pre1 =  document.getElementById("precio1");
+    contaClicks ++;
+    img1.innerHTML =` <img src="sudadera5.jpg"/>`;
+    pre1.innerHTML = "$52.99";
+}
